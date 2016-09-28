6ee9a27ce9b009c1425f16e66deb5fe4b5cb9039
Sherman-Chen <shc410@gmail.com>
2016-09-27 20:18:36 -0700

    In progress - rooms
    - Buggy new room logic

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,7 @@
 var friendsList = [];
 var messages = '';
 var roomName = 'lobby';
+var rooms = {};
 
 app = {
   server: 'https://api.parse.com/1/classes/messages',
@@ -19,7 +20,7 @@ app = {
     });
 
     //fetch initial feed
-    this.fetch();
+    app.fetch();
 
   //  setInterval(function() { app.fetch(); }, 1000);
   },
@@ -34,7 +35,7 @@ app = {
       success: function(data) {
         console.log('Msg sent');
         console.log('data: ', data);
-        app.fetch();
+        app.fetch(roomName);
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -42,7 +43,7 @@ app = {
     });
   },
   // fetch all messages
-  fetch: function (username, room) {
+  fetch: function (room) {
 
     $.ajax({
       url: this.server,
@@ -50,6 +51,10 @@ app = {
       dataType: 'json', //what does does jsonp do?
       success: function(responses) {
         messages = responses.results;
+        console.log(messages);
+        if (!room) {
+          app.getRoom();
+        }
         app.renderMessage(messages);
       },
       error: function(data) {
@@ -58,6 +63,7 @@ app = {
       data: {
         limit: 30,
         order: '-createdAt',
+        where: {"roomname": room}
       },
       contentType: 'application/json',
     });
@@ -89,7 +95,7 @@ app = {
   },
   // helper function to render messages on screen
   renderMessage: function (messages) {
-    $('#chats').html('');
+    app.clearMessages();
     messages.forEach(function(message) {
       var $message = '<div class="chat"><span class="username">' + _.escape(message.username) 
                   + '</span>:<br><span class="message">' + _.escape(message.text) + '</span></div>';
@@ -120,8 +126,9 @@ app = {
   // filter messages for roomlist
   getRoom: function () {
     console.log ('inside getRoom');
-    var rooms = {};
-    console.log(messages);
+    $('#rooms').html('<option value="__newRoom">New Room...</option>');
+    $('#rooms').append('<option value="">All</option>');
+
     messages.forEach(function(message) {
       var roomName = message.roomname;
       // if roomName exists on JSON response, and has not been added to rooms yet
@@ -132,20 +139,31 @@ app = {
     });
   },
 
+  filterMessages: function(messages, roomName) {
+    messages.filter(function(message) {
+      return message.roomname === roomName;
+    });
+  },
+
   //get messages for selected room
   handleRoomChange: function () {
     // get current room
     roomName = $('#rooms').val();
     // render for current room
-    var roomMessages = messages.filter(function(message) {
-      if (message === roomName) {
-        return true;
-      }
-    });
+    var selectedIndex = $('#rooms').prop('selectedIndex');
 
-    app.renderMessage(roomMessages);
-
-  }
+    if (selectedIndex === 0) {
+      var name = prompt ('Enter room name');
+      rooms[roomName] = true;
+      app.renderRoom(roomName);
+    }
+    console.log(roomName);
+    if (roomName !== "") {
+     app.fetch(roomName);
+    } else {
+      app.fetch();
+    }
+  },
 
 
 };
